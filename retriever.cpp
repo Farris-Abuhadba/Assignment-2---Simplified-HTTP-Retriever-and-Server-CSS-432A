5739587e4af28603af41a2097275edc8c7b99c8f
Farris <farriswalid123@gmail.com>
2026-02-05 18:36:04 -0800

    Added file download and changed testing url to http://httpforever.com/

--- retriever.cpp
+++ retriever.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <fstream>
 #include <sstream>
 #include <string>
 #include <cstring>
@@ -21,7 +22,7 @@ int main(int argc, char* argv[])
     std::string path = "/";
     int port = 80;
 
-    // Remove http:// if present
+    // Strip http:// if present
     if (url.find("http://") == 0) {
         url = url.substr(7);
     }
@@ -42,41 +43,95 @@ int main(int argc, char* argv[])
         return 1;
     }
 
-    // Create socket
     int sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0) {
         perror("socket");
         return 1;
     }
 
-    // Build server address
     sockaddr_in serv_addr;
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(port);
     memcpy(&serv_addr.sin_addr, server->h_addr, server->h_length);
 
-    // Connect
     if (connect(sockfd, (sockaddr*)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("connect");
         close(sockfd);
         return 1;
     }
 
-    // Build HTTP GET request
-    std::ostringstream request;
-    request << "GET " << path << " HTTP/1.0\r\n" << "Host: " << host << "\r\n" << "Connection: close\r\n\r\n";
+    std::string request =
+        "GET " + path + " HTTP/1.0\r\n" +
+        "Host: " + host + "\r\n" +
+        "Connection: close\r\n\r\n";
 
-    // Send request
-    write(sockfd, request.str().c_str(), request.str().length());
+    write(sockfd, request.c_str(), request.length());
 
-    // Read and print response
+    // Read headers first
+    std::string headers;
     char buffer[BUFFER_SIZE];
     int bytes;
-    while ((bytes = read(sockfd, buffer, BUFFER_SIZE)) > 0) {
-        std::cout.write(buffer, bytes);
+
+    while (headers.find("\r\n\r\n") == std::string::npos) {
+        bytes = read(sockfd, buffer, BUFFER_SIZE);
+        if (bytes <= 0) {
+            std::cerr << "Failed to read HTTP headers\n";
+            close(sockfd);
+            return 1;
+        }
+        headers.append(buffer, bytes);
+    }
+
+    size_t header_end = headers.find("\r\n\r\n");
+    std::string header_part = headers.substr(0, header_end);
+    std::string body = headers.substr(header_end + 4);
+
+    // Parse status line
+    std::istringstream header_stream(header_part);
+    std::string status_line;
+    getline(header_stream, status_line);
+
+    // Parse Content-Length
+    std::string line;
+    int content_length = -1;
+    while (getline(header_stream, line)) {
+        if (line.rfind("Content-Length:", 0) == 0) {
+            content_length = atoi(line.substr(15).c_str());
+            break;
+        }
+    }
+
+    if (content_length < 0) {
+        std::cerr << "No Content-Length found\n";
+        close(sockfd);
+        return 1;
+    }
+
+    // Read rest of body
+    while ((int)body.size() < content_length) {
+        bytes = read(sockfd, buffer, BUFFER_SIZE);
+        if (bytes <= 0) break;
+        body.append(buffer, bytes);
     }
 
     close(sockfd);
+
+    // If not 200 OK, print error body
+    if (status_line.find("200") == std::string::npos) {
+        std::cout << body;
+        return 0;
+    }
+
+    // Determine output filename
+    std::string filename = path;
+    if (filename == "/") filename = "index.html";
+    else if (filename[0] == '/') filename = filename.substr(1);
+
+    std::ofstream outfile(filename, std::ios::binary);
+    outfile.write(body.c_str(), content_length);
+    outfile.close();
+
+    std::cout << "Saved file: " << filename << std::endl;
     return 0;
 }
